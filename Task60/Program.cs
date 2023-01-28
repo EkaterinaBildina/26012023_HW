59abee328cd607a9bface4d22b440cbe8e530508
EkaterinaBildina <kukresch@gmail.com>
2023-01-29 01:16:10 +0300

    task60 success. final review commit

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -20,6 +20,16 @@ int[,,] Create3DMatrixRndInt(int rows, int columns, int volume, int min, int max
             for (int k = 0; k < matrix3D.GetLength(2); k++)
             {
                 matrix3D[i, j, k] = rnd.Next(min, max + 1);
+
+                for (int a = 1; a < k; a++)
+                {
+                    if (matrix3D[i, j, a] == matrix3D[i, j, a+1])
+                        matrix3D[i, j, a+1] = rnd.Next(min, max + 1);
+                        if (matrix3D[i, a, k] == matrix3D[i, a+1, k])
+                        matrix3D[i, a+1, k] = rnd.Next(min, max + 1);
+                            if (matrix3D[a, j, k] == matrix3D[a+1, j, k])
+                            matrix3D[a+1, j, k] = rnd.Next(min, max + 1);
+                }
             }
         }
     }
@@ -31,7 +41,7 @@ void PrintMatrix(int[,,] matrix)
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         Console.Write("[");
-        for (int j = 0; j < matrix.GetLength(1)-1; j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
             for (int k = 0; k < matrix.GetLength(2); k++)
             {
@@ -44,9 +54,18 @@ void PrintMatrix(int[,,] matrix)
     }
 }
 
+bool NumberDiapasoneReview(int rows, int columns, int volume)
+{
+    return rows * columns * volume <= 99;
+}
 
-int[,,] matrix3D = Create3DMatrixRndInt(3, 3, 3, 10, 99);
-PrintMatrix(matrix3D);
+if (NumberDiapasoneReview(2, 2, 2))
+{
+    int[,,] matrix3D = Create3DMatrixRndInt(2, 2, 2, 10, 99);
+    PrintMatrix(matrix3D);
+}
+else Console.WriteLine("В заданном размере 3Д-матрицы невозможно вывести неповторяющиеся двузначные числа");
+
 
 
 
